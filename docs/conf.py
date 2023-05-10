3a9e6fba6a4fe32d978a14c4f7097496def22677
devanshshukla99 <devanshshukla99@outlook.com>
2023-05-10 12:18:27 +0530

    excluding docs/changes from sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ numpydoc_show_class_members = False
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["changes"]
 
 
 # -- Options for HTML output -------------------------------------------------
