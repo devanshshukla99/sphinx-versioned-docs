d5b4ae42682f753389e630e3bd16d45aedf41269
Devansh Shukla <devanshshukla99@outlook.com>
2024-03-17 16:17:22 +0530

    testing infra for branch selection

---
+++ tests/test_branch_selection.py
@@ -0,0 +1,62 @@
+import os
+import pytest
+import pathlib
+from bs4 import BeautifulSoup as bs
+
+VERSIONS_SUPPOSED = {
+    "v1.0": [
+        "index.html",
+    ],
+    "main": ["index.html", "example.html", "code_ref/api_ref/example2.html"],
+}
+BASEPATH = pathlib.Path(os.getcwd()) / "docs"
+OUTPATH = BASEPATH / "_build"
+
+
+def test_top_level_index():
+    assert OUTPATH.exists()
+    assert (OUTPATH / "index.html").is_file()
+    return
+
+
+def test_branch_selection():
+    # v1.0 and main should exist, v2.0/anyother shouldn't
+    versions_should_exist = list(VERSIONS_SUPPOSED.keys())
+    # `versions_should_exist` plus top-level index.html
+    versions_should_exist.append("index.html")
+    # get files and folders in the outpath
+    outpath_glob = OUTPATH.glob("*")
+    for filefolder in outpath_glob:
+        assert filefolder.name in versions_should_exist
+    return
+
+
+@pytest.mark.parametrize("ver, file", [(x, z) for x, y in VERSIONS_SUPPOSED.items() for z in y])
+def test_injected_hyperlinks(ver, file):
+    assert (OUTPATH / ver / file).is_file()
+
+    data = None
+    with open(OUTPATH / ver / file) as f:
+        data = f.read()
+    soup = bs(data, features="html.parser")
+    injected_code = soup.find_all(class_="injected")
+
+    # Injected code must exist
+    assert injected_code
+
+    for inj in injected_code:
+        # All hyper-links in the injected code
+        hyperlinks = inj.find_all("a")
+        hyperlinks_text = set(x.text for x in hyperlinks)
+
+        # Makesure all versions exist in hyperlinks
+        assert set(VERSIONS_SUPPOSED.keys()) == hyperlinks_text
+        print(f"Versions found in soup: {hyperlinks_text}")
+
+        # Test hyperlinks
+        for link in hyperlinks:
+            url = pathlib.Path(link.attrs.get("href"))
+            filepath = (OUTPATH / ver / file).parent / url
+            assert filepath.is_file()
+
+    return
